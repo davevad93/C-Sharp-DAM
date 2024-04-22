d1a223b405ffa5e42c194536527fb9eb3fed434f
Davide Presti <94308234+davevad93@users.noreply.github.com>
2024-04-22 16:35:39 +0200

    Add 'enumeraciones' project

---
+++ proyectos/parte 2/enumeraciones/ejercicio 3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+// DAVIDE PRESTI
+// - Ejercicio 3 -
+// Crea un método genérico llamado LeeEnum, que permitirá recoger un string del usuario hasta que coincida con un elemento de la enumeración X. 
+// El método tendrá que comprobar si la string introducida por el usuario pertenece a la enumeración, 
+// si pertenece devolverá el elemento de la enumeración que coincida, si nó mostrará todos los elementos de la enumeración y volverá a pedir el string al usuario.
+// Este método tendrá la siguiente signatura:
+
+// public static Object LeerEnum(Type tipo, string texto, string textoError);
+// Donde:
+
+// tipo: El tipo de la enumeración obtenido con la función typeof(IdentificadorEnumeracion).
+// texto: El texto que se muestra para pedir al usuario que introduzca el valor de la enumeración.
+// textoError: El texto que se mostrará si el valor que se introduce no pertenece a la enumeración.
+// Modifica el Ejercicio 2 para usar el método LeeEnum a la hora de recoger el tipo de Abono a comprar.
+
+// 👉Pista: Este método utilizará Enum.IsDefined para comprobar si el valor pertenece a la enumeración, 
+// Enum.Parse para convertir el string a tipo Enum y Enum.GetNames para crear el array que se mostrará en caso de que el dato sea erróneo.
+
+namespace ejercicio3
+{
+    class Program
+    {
+        enum TipoAbono
+        {
+            QuinceDias = 70, 
+            TreintaDias = 60, 
+            FamiliasNumerosas = 50,
+            TerceraEdad = 30, 
+            Discapacidad = 20, 
+            Juvenil = 65, 
+            Infantil = 35, 
+            Turistico = 90
+        }
+
+        static Object LeerEnum(Type tipo, string texto, string textoError)
+        {
+            Object valorEnum = null;
+            bool entradaValida;
+            do
+            {
+                Console.Write(texto);
+                string entradaUsuario = Console.ReadLine();
+                entradaValida = Enum.IsDefined(tipo, entradaUsuario);
+
+                if (entradaValida)
+                {
+                    valorEnum = Enum.Parse(tipo, entradaUsuario);
+                }
+                    
+                else
+                {
+                    Console.Write(textoError);
+                    string salida = "\nSólo se aceptan abonos de tipo:\n" + string.Join(", ", Enum.GetNames(tipo));
+                    Console.WriteLine(salida);
+                }
+            }
+            while (entradaValida == false);
+            return valorEnum;
+        }
+
+        static int LeeDias()
+        {
+            int numeroDias;
+            const int MINIMO_DIAS = 7;
+            const int MAXIMO_DIAS = 60;
+
+            do
+            {
+                Console.Write("\nPara cuántos días quiere el abono? ");
+                numeroDias = int.Parse(Console.ReadLine());
+                if (numeroDias >= 7 && numeroDias <= 60) return numeroDias;
+                Console.WriteLine($"\nERROR! Valor no válido, los abonos deben ser de {MINIMO_DIAS} a {MAXIMO_DIAS} días.");
+            } 
+            while (true);
+        }
+
+        static int Dias(TipoAbono abono)
+        {
+            return abono switch
+            {
+                TipoAbono.QuinceDias => 15,
+                TipoAbono.TreintaDias => 30,
+                _ => LeeDias()
+            };
+        }        
+
+        static string CalculaCoste(TipoAbono abono, int numeroDias)
+        {
+            string salida = "\nEl total del precio del abono es de " ;
+            salida += abono switch 
+            {
+                TipoAbono.QuinceDias => (70f / 100f * numeroDias).ToString(),
+                TipoAbono.TreintaDias => (60f / 100f * numeroDias).ToString(),
+                TipoAbono.FamiliasNumerosas => (50f / 100f * numeroDias).ToString(),
+                TipoAbono.TerceraEdad => (30f / 100f * numeroDias).ToString(),
+                TipoAbono.Discapacidad => (20f / 100f * numeroDias).ToString(),
+                TipoAbono.Juvenil => (65f / 100f * numeroDias).ToString(),
+                TipoAbono.Infantil => (35f / 100f * numeroDias).ToString(),
+                TipoAbono.Turistico => (90f / 100f * numeroDias).ToString(),
+                _ => (120f / 100f * numeroDias).ToString()
+            };
+            return salida;
+        }
+
+        static void Main(string[] args)
+        {
+            string textoPrueba = "\nIntroduzca el tipo de abono: ";
+            string textoError = "\nERROR! Tipo de abono inexistente.\n";
+            TipoAbono abono = (TipoAbono)LeerEnum(typeof(TipoAbono), textoPrueba, textoError);
+            Console.WriteLine($"{CalculaCoste(abono, LeeDias())} euros.\n");
+        }
+    }
+}
---
+++ proyectos/parte 2/enumeraciones/ejercicio 5/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+// DAVIDE PRESTI
+// - Ejercicio 5 -
+// Crea una aplicación para gestionar la personalización de coches en un determinado taller.
+
+// Tendrás que utilizar Enumeraciones NO excluyentes, 
+// debes definir la enumeración con un mínimo de 7 colores (incluido el None).
+// La aplicación permitirá añadir un color o más a la elección, 
+// eliminar un color de los que ya se habían elegido y mostrar los colores elegidos.
+// El programa comenzará mostrando un menú, con las tres opciónes y la que nos permita salir.
+// Nota: Deberás usar el método LeeEnum, para introducir los datos que se piden al usuario 
+// y tendrás que crear, como mínimo, un método para cada una de las posibles opciones del menú.
+
+namespace ejercicio5
+{
+    class Program
+    {
+        [Flags]
+        enum ColoresCoche
+        {
+            None = 0b_0000_0000,
+            Negro = 0b_0000_0001,
+            Blanco = 0b_0000_0010,
+            Gris = 0b_0000_0100,
+            Rojo = 0b_0000_1000,
+            Naranja = 0b_0001_0000,
+            Amarillo = 0b_0010_0000,
+            Verde = 0b_0100_0000,
+        }
+
+        static Object LeerEnum(Type tipo, string texto, string textoError)
+        {
+            Object valorEnum;
+            bool entradaValida;
+            do
+            {
+                string valoresValidos = string.Join(", ", Enum.GetNames(tipo));
+                string entradaUsuario;
+
+                Console.Write($"{texto} ({valoresValidos}): ");
+                entradaUsuario = Console.ReadLine();
+                entradaValida = Enum.IsDefined(tipo, entradaUsuario);
+
+                if (entradaValida)
+                    valorEnum = Enum.Parse(tipo, entradaUsuario);
+                else
+                {
+                    valorEnum = null;
+                    Console.WriteLine($"{textoError}, sólo se aceptan los siguientes valores:\n{valoresValidos}\n");
+                }
+            }
+            while (entradaValida == false);
+            return valorEnum;
+        }
+
+        static ColoresCoche LeeColor()
+        {
+            string texto = "Selecciona un color:";
+            string textoError = "\nColor seleccionado incorrecto";
+            return (ColoresCoche)LeerEnum(typeof(ColoresCoche), texto, textoError);
+        }
+
+        static ColoresCoche AñadeColor(ColoresCoche estado)
+        {
+            estado |= LeeColor();
+            return estado;
+        }
+
+        static ColoresCoche QuitaColor(ColoresCoche estado)
+        {
+            estado &= ~LeeColor();
+            return estado;
+        }
+
+        static void MuestraColores(ColoresCoche estado)
+        {
+            Console.WriteLine($"Colores seleccionados: {estado} ({Convert.ToString((byte)estado, 2).PadLeft(8, '0')})\n");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            ColoresCoche estado = default;
+            bool salir = true;
+            
+            do
+            {
+                Console.WriteLine("\n<|>|<|> MENÚ COLORES <|>|<|>\n" +
+                                  "\n1. Añadir color.\n2. Quitar color." +
+                                  "\n3. Mostrar colores seleccionados.\nESC. Salir.\n");
+                var tecla = Console.ReadKey(true);
+                salir = tecla.Key == ConsoleKey.Escape;
+
+                switch (tecla.KeyChar)
+                {
+                    case '1':
+                            estado = AñadeColor(estado);
+                            break;
+                    case '2':
+                            estado = QuitaColor(estado);
+                            break;
+                    case '3':
+                            MuestraColores(estado);
+                            break;
+                    default:
+                        if (tecla.Key == ConsoleKey.Escape)
+                            Console.WriteLine("Programa finalizado.\n");
+                        else
+                            Console.WriteLine("ERROR! Opción inexistente.\n");
+                            break; 
+                }
+            }
+            while (!salir);
+        }
+    }
+}
